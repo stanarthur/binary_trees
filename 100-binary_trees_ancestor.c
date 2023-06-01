76e21986e910bafe1f4d4f1f9eb482578aca512e
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-06-01 15:12:04 +0100

    19. Lowest common ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,64 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common
+ *                          ancestor of two nodes.
+ * @first: A pointer to the first node.
+ * @second: A pointer to the second node.
+ *
+ * Return: A pointer to the lowest common ancestor node.
+ *         NULL if there is no common ancestor.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	size_t depth_first, depth_second;
+
+	if (!first || !second)
+		return (NULL);
+
+	depth_first = binary_tree_depth(first);
+	depth_second = binary_tree_depth(second);
+
+	while (depth_first > depth_second)
+	{
+		first = first->parent;
+		depth_first--;
+	}
+	while (depth_second > depth_first)
+	{
+		second = second->parent;
+		depth_second--;
+	}
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+	return ((binary_tree_t *)first);
+}
+
+/**
+ * binary_tree_depth - Measures the depth of a binary tree.
+ * @tree: Pointer to the binary tree to measure.
+ *
+ * Return: The depth of the tree.
+ *         0 if the tree is NULL.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -18,7 +18,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}binary_tree_t;
+} binary_tree_t;
 
 /* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
@@ -43,7 +43,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const
+									binary_tree_t *second);
 
 /* Helper functions*/
 int _pow_recursion(int x, int y);

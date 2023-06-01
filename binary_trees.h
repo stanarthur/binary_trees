376485da1dc4b7aa9ebf791e10ccdfef4a2111ca
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-06-01 15:30:05 +0100

    21. Is complete

--- binary_trees.h
+++ binary_trees.h
@@ -46,9 +46,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const
 									binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
 
 /* Helper functions*/
 int _pow_recursion(int x, int y);
 void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
+int btic_helper(const binary_tree_t *tree, size_t index, size_t size);
 
 #endif /* _BINARY_TREES_H_ */

3b61146fdffe3f4fad1c672c7e45f9780af0f6c4
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-06-03 03:42:51 +0100

    133

--- binary_trees.h
+++ binary_trees.h
@@ -68,6 +68,7 @@ avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
 
 /* prototype functions for task 28 */
 bst_t *inorder_successor(bst_t *root);
